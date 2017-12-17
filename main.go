fd0939627f5d33348af5ceaa9a7364d586340bc9
hendrikhoffmann <hhoffmann@web.de>
2017-12-17 13:10:22 +0100

    add initial routing

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"net/http"
-
-	"golang.org/x/net/websocket"
-	"github.com/hendrikhoffmann/smartfblib"
+ //   "encoding/json"
+    "log"
+    "github.com/gorilla/mux"
+ 	"github.com/hendrikhoffmann/smartfblib"
 )
 
 func main() {
-	http.Handle("/switchon", websocket.Handler(func(ws *websocket.Conn) {
-		ws.Write([]byte(smartfblib.SwitchOn()))
-	}))
-	http.ListenAndServe(":3000", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/switchon", smartfblib.SwitchOn).Methods("GET")
+	router.HandleFunc("/switchoff", smartfblib.SwitchOff).Methods("GET")
+    router.HandleFunc("/switchchannel/{channel}", smartfblib.SwitchChannel).Methods("GET")
+    router.HandleFunc("/setvolume/{volume}", smartfblib.SetVolume).Methods("GET")
+    log.Fatal(http.ListenAndServe(":3000", router))
+
 }
+
+//import	"golang.org/x/net/websocket"
+//Websocket Communication Postponed
+//	http.Handle("/switchon", websocket.Handler(func(ws *websocket.Conn) {
+//		ws.Write([]byte(smartfblib.SwitchOn()))
+//	}))
+//	http.ListenAndServe(":3000", nil)
